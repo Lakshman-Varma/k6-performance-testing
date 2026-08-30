6a8829a4e6b69935ff6a606516b04bd5ca1770b3
Lakshman-Varma <lakshmanvarmasangadi@gmail.com>
2026-08-30 20:00:30 +0530

    add Test3 k6 test

---
+++ k6-login-test/Test3.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+
+  check(res, {
+    'Status is 200': (r) => r.status === 200,
+    'Response time < 1000ms': (r) => r.timings.duration < 1000,
+  });
+
+  sleep(1);
+}
